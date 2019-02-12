b36311b4a19bedab4f4a48360283a01844832baa
akshayr20 <akshay.rajput5394@gmail.com>
2019-02-12 08:14:57 +0530

    error handling

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-	next();
-});
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 const productsRoute = require('./api/routes/products');
 const ordersRoute = require('./api/routes/products');
 
 app.use('/products', productsRoute);
 app.use('/orders', ordersRoute);
+
+app.use((req, res, next) => {
+	const error = new Error('Not Found');
+	error.status = 404;
+	next(error);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	res.json({
+		errors: {
+			message: error.message
+		}
+	});
+});
 
 module.exports = app;
