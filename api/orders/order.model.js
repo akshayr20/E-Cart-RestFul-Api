a399d13642f3c456724fad9e384b42e32b541e2f
akshayr20 <akshay.rajput5394@gmail.com>
2019-02-18 11:10:26 +0530

    Changes in responses for product API, (readabilty)

--- api/orders/order.model.js
+++ api/orders/order.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = mongoose.Schema({
 	_id: mongoose.Schema.Types.ObjectId,
 	product: mongoose.Schema.Types.ObjectId,
-	quantity: Number
+	quantity: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
--- api/products/product.controller.js
+++ api/products/product.controller.js
@@ -3,8 +3,23 @@ const mongoose = require('mongoose');
 
 module.exports.getAllProducts = async (req, res) => {
 	try {
-		const product = await Product.find();
-		res.status(200).json(product);
+		const product = await Product.find().select('name price _id');
+		const response = {
+			count: product.length,
+			products: product.map(doc => {
+				return {
+					id: doc._id,
+					name: doc.name,
+					price: doc.price,
+					request: {
+						type: 'GET',
+						description: 'PRODUCT_INFO',
+						url: `http://localhost:3000/products/${doc._id}`
+					}
+				};
+			})
+		};
+		res.status(200).json(response);
 	} catch (error) {
 		res.status(500).json(error);
 	}
@@ -13,11 +28,19 @@ module.exports.getAllProducts = async (req, res) => {
 module.exports.getProductById = async (req, res) => {
 	try {
 		const id = req.params.id;
-		const product = await Product.findById(id);
+		const product = await Product.findById(id).select('name price');
+		const response = {
+			product,
+			request: {
+				type: 'GET',
+				description: 'GET_ALL_PRODUCTS',
+				url: `http://localhost:3000/products`
+			}
+		};
 		if (product) {
-			res.status(200).json(product);
+			res.status(200).json(response);
 		} else {
-			res.status(404).json({ errorMessage: 'No Products found from the given Id' });
+			res.status(404).json({ errorMessage: 'NO_PRODUCT_FOUND' });
 		}
 	} catch (error) {
 		res.status(500).json({ error });
@@ -32,7 +55,15 @@ module.exports.createProduct = async (req, res) => {
 			price: req.body.price
 		});
 		const result = await product.save();
-		res.status(200).json(result);
+		const response = {
+			message: 'PRODUCT_CREATED',
+			request: {
+				type: 'GET',
+				description: 'GET_PRODUCT_INFO',
+				url: `http://localhost:3000/products/${result._id}`
+			}
+		};
+		res.status(200).json(response);
 	} catch (error) {
 		res.status(500).json(error);
 	}
@@ -44,8 +75,16 @@ module.exports.updateProductById = async (req, res) => {
 		const updatedProduct = {
 			$set: req.body
 		};
-		const result = await Product.update({ _id: id }, updatedProduct);
-		res.status(200).json(result);
+		await Product.update({ _id: id }, updatedProduct);
+		const response = {
+			message: 'PRODUCT_UPDATED',
+			request: {
+				type: 'GET',
+				description: 'GET_PRODUCT_INFO',
+				url: `http://localhost:3000/products/${id}`
+			}
+		};
+		res.status(200).json(response);
 	} catch (error) {
 		res.status(500).json(error);
 	}
@@ -54,8 +93,16 @@ module.exports.updateProductById = async (req, res) => {
 module.exports.deleteProductById = async (req, res) => {
 	try {
 		const id = req.params.id;
-		const product = await Product.remove({ _id: id });
-		res.status(200).json(product);
+		await Product.remove({ _id: id });
+		const response = {
+			message: 'PRODUCT_DELETED',
+			request: {
+				type: 'GET',
+				description: 'GET_ALL_PRODUCTS',
+				url: `http://localhost:3000/products`
+			}
+		};
+		res.status(200).json(response);
 	} catch (error) {
 		res.status(500).json(error);
 	}
